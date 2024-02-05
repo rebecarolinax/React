9ac671ed1b8f24e9325ac54b3f850ce1de875583
Rebeca <rebecacarolina75@gmail.com>
2024-02-05 15:12:20 -0300

    React

---
+++ hello/src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const LoginPage = () => {
+  return <>
+  <h1>Página de Login</h1></>;
+};
+
+export default LoginPage;
---
+++ hello/src/pages/ProdutosPage/ProdutosPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ProdutosPage = () => {
+  return <div>
+    <h1>Página de Produtos</h1>
+  </div>;
+};
+
+export default ProdutosPage;
